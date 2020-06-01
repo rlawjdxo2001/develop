3555457f97dc918d1a61300f81ffff527702a417
Raisin03 <Raisin03@10.0.100.32>
2020-06-01 17:04:23 +0900

    【共通】中間コミット

---
+++ src/jp/co/preProcess/cmnChkPreProcess.java
@@ -0,0 +1,43 @@
+package jp.co.preProcess;
+
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+public class cmnChkPreProcess extends AbstractInterceptor{
+
+    /* ロガー */
+    private Logger logger = LogManager.getLogger(cmnChkPreProcess.class);
+
+
+	@Override
+	public String intercept(ActionInvocation invocation) throws Exception {
+		logger.info("----------------start {}.{}----------------", "cmnChkPreProcess", "intercept");
+
+		//セッション情報の有無をチェックする。
+		if(chkSession(invocation)) {
+			return "goLogin";
+		}
+
+		logger.info("---------------- end {}.{} ----------------", "cmnChkPreProcess", "intercept");
+		return invocation.invoke();
+	}
+
+	private boolean chkSession(ActionInvocation invocation) {
+		logger.info("----------------start {}.{}----------------", "cmnChkPreProcess", "chkSession");
+		//セッション情報を取得する。
+		Map<String, Object> session = invocation.getInvocationContext().getSession();
+		if (session.get("userSession") == null) {
+			logger.info("セッション情報がありません、ログイン画面に戻ります。");
+			logger.info("---------------- end {}.{} ----------------", "cmnChkPreProcess", "chkSession");
+			return true;
+		} else {
+			logger.info("---------------- end {}.{} ----------------", "cmnChkPreProcess", "chkSession");
+			return false;
+		}
+	}
+}
